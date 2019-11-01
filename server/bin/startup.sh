a01b5e61f82bc71c285aa216ef73745915ea7845
695199262 <695199262@qq.com>
2019-11-01 17:28:44 +0800

    修改脚本

--- server/bin/startup.sh
+++ server/bin/startup.sh
@@ -19,7 +19,7 @@ JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
 echo "Java version: " $JAVA_VERSION
 
 if [ "$1" == "-daemon" ]; then
-    nohup java -server -Xbootclasspath/a:../conf:../ -cp ../lib/flyingsocks-server-2.0.jar >/dev/null com.lzf.flyingsocks.server.ServerBoot &
+    nohup java -server -Xbootclasspath/a:../conf:../ -cp ../lib/flyingsocks-server-2.0.jar com.lzf.flyingsocks.server.ServerBoot >/dev/null
 else
     java -server -Xbootclasspath/a:../conf:../ -cp ../lib/flyingsocks-server-2.0.jar com.lzf.flyingsocks.server.ServerBoot
 fi
