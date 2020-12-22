3f1e5180b795f0292f8d1e5b89b0c5bb85e7357c
lizifan <695199262@qq.com>
2020-12-22 16:38:38 +0800

    [v3.0] client update

---
+++ client/src/test/java/com/lzf/flyingsocks/test/SWTDemo.java
@@ -0,0 +1,12 @@
+package com.lzf.flyingsocks.test;
+
+import org.jfree.experimental.chart.swt.demo.SWTBarChartDemo1;
+import org.jfree.experimental.chart.swt.demo.SWTTimeSeriesDemo;
+
+public class SWTDemo {
+
+    public static void main(String[] args) {
+        SWTTimeSeriesDemo.main(args);
+    }
+
+}
